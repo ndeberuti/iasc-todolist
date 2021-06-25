322fb61bd510b130f1c91d181384dc30b90dd45e
Nicolas de Beruti <ndeberuti@gmail.com>
2021-06-25 19:58:19 -0300

    linter implementation and some fixes

--- src/server/entities/task.js
+++ src/server/entities/task.js
@@ -1,54 +1,53 @@
 class Task {
-    constructor(value, lastTask, nextTask) {
-        this.value = value;
-        this.lastTask = lastTask;
-        this.nextTask = nextTask;
+  constructor(value, lastTask, nextTask) {
+    this.value = value;
+    this.lastTask = lastTask;
+    this.nextTask = nextTask;
+  }
+
+  putBefore(task) {
+    if (this != task) {
+      // Desvinculamos la task de sus posición actual
+      this.unlinkTask(task);
+
+      // Se hubica la task en la posición deseada
+      this.lastTask = task.lastTask;
+      this.nextTask = task;
+
+      // Modicamos la tasks necesarias
+      if (task.lastTask != null) {
+        (task.lastTask).nextTask = this;
+      }
+      task.lastTask = this;
     }
+  }
 
-    putBefore(task) {
-        if (this != task) {
-            //Desvinculamos la task de sus posición actual
-            this.unlinkTask(task);
+  putAfter(task) {
+    if (this != task) {
+      // Desvinculamos la task de sus posición actual
+      this.unlinkTask(task);
 
-            //Se hubica la task en la posición deseada
-            this.lastTask = task.lastTask;
-            this.nextTask = task;
+      // Se hubica la task en la posición deseada
+      this.nextTask = task.nextTask;
+      this.lastTask = task;
 
-            //Modicamos la tasks necesarias
-            if (task.lastTask != null) {
-                (task.lastTask).nextTask = this
-            }
-            task.lastTask = this;
-        }
+      // Modicamos la tasks necesarias
+      if (task.nextTask != null) {
+        (task.nextTask).lastTask = this;
+      }
+      task.nextTask = this;
     }
+  }
 
-    putAfter(task) {
-        if (this != task) {
-            //Desvinculamos la task de sus posición actual
-            this.unlinkTask(task);
-
-            //Se hubica la task en la posición deseada
-            this.nextTask = task.nextTask;
-            this.lastTask = task;
-
-            //Modicamos la tasks necesarias
-            if (task.nextTask != null) {
-                (task.nextTask).lastTask = this
-            }
-            task.nextTask = this;
-        }
+  // Desvinculamos la task de sus posición actual
+  unlinkTask(task) {
+    const last = this.lastTask;
+    const next = this.nextTask;
+    if (last != null) {
+      last.nextTask = next;
     }
-
-    //Desvinculamos la task de sus posición actual
-    unlinkTask(task) {
-        let last = this.lastTask;
-        let next = this.nextTask;
-        if (last != null) {
-            last.nextTask = next;
-        }
-        if (next != null) {
-            next.lastTask = last;
-        }
+    if (next != null) {
+      next.lastTask = last;
     }
-
+  }
 }
--- src/server/entities/todoList.js
+++ src/server/entities/todoList.js
@@ -1,69 +1,71 @@
 class TodoList {
-    constructor(tasks) {
-        this.tasks = tasks;
-        this.isPublic = false;
+  constructor(tasks) {
+    this.tasks = tasks;
+    this.isPublic = false;
+  }
+
+  removeTask(task) {
+    const pos = this.tasks.indexOf(task);
+    // Avoid remove other task
+    if (pos >= 0) {
+      this.tasks.splice(pos, 1);
     }
+    task.unlinkTask();
+  }
 
-    removeTask(task) {
-        const pos = this.tasks.indexOf(task);
-        //Avoid remove other task
-        if (pos >= 0) {
-            this.tasks.splice(pos, 1);
-        }
-        task.unlinkTask();
+  pushTask(task) {
+    // Unlink Task
+    task.unlinkTask();
+
+    // Avoid re-insert an existed task
+    const exist = this.tasks.indexOf(task) >= 0;
+    if (!exist) {
+      const sortedTasks = this.getSortedTasks();
+      const lastTaks = this.tasks[this.tasks.length - 1];
+      if (lastTaks != null) {
+        task.putAfter(lastTaks);
+      }
+      this.tasks.push(task);
     }
+  }
 
-    pushTask(task) {
-        //Unlink Task
-        task.unlinkTask();
+  getSortedTasks() {
+    return this.tasks.sort((a, b) => {
+      if (a.lastTask == null) {
+        return -1;
+      }
 
-        //Avoid re-insert an existed task
-        const exist = this.tasks.indexOf(task) >= 0;
-        if (!exist) {
-            const sortedTasks = this.getSortedTasks();
-            const lastTaks = this.tasks[this.tasks.length - 1];
-            if (lastTaks != null) {
-                task.putAfter(lastTaks);
-            }
-            this.tasks.push(task);
-        }
-    }
+      if (b.lastTask == null) {
+        return 1;
+      }
 
-    getSortedTasks() {
-        return this.tasks.sort(function (a, b) {
-            if (a.lastTask == null) {
-                return -1;
-            }
+      if (a.nextTask == b) {
+        return -1;
+      }
 
-            if (b.lastTask == null) {
-                return 1;
-            }
+      if (a.lastTask == b) {
+        return 1;
+      }
 
-            if (a.nextTask == b) {
-                return -1;
-            }
+      if (b.lastTask == a) {
+        return -1;
+      }
 
-            if (a.lastTask == b) {
-                return 1;
-            }
+      if (b.nextTask == a) {
+        return 1;
+      }
 
-            if (b.lastTask == a) {
-                return -1;
-            }
+      if (a.nextTask == null) {
+        return 1;
+      }
 
-            if (b.nextTask == a) {
-                return 1;
-            }
+      if (b.nextTask == null) {
+        return -1;
+      }
 
-            if (a.nextTask == null) {
-                return 1;
-            }
-
-            if (b.nextTask == null) {
-                return -1;
-            }
-
-            return 0;
-        });
-    }
+      return 0;
+    });
+  }
 }
+
+module.exports = TodoList;
