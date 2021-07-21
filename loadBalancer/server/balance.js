4fcb68ace8da90634d8894a1e14964d9e6b432ac
Nicolas de Beruti <nicolas.deberuti.ar@socialbakers.com>
2021-07-21 19:39:18 -0300

    Agregar pickeo de shard simplificado con comentario para referencia

--- loadBalancer/server/balance.js
+++ loadBalancer/server/balance.js
@@ -1,26 +1,26 @@
-const routingTable = require('./routingTable');
 const shards = require('./shards.json');
 
-const pickShard = (req) => {
-  let body = '';
-  let key;
-  req.on('data', (chunk) => {
-    body += chunk;
-  });
-  req.on('end', () => {
-    const parsedBody = JSON.parse(body);
-    // TODO definir que va aca
-    key = parsedBody.key;
-  });
+const pickShard = (req) => (req.headers.ispublicmode ? shards.shard1 : shards.shard2);
+// let body = '';
+// let shard = null;
 
-  if (routingTable.has(key)) {
-    return routingTable.get(key);
-  }
+// req.on('data', (chunk) => {
+//   body += chunk;
+// });
+// await req.on('end', async () => {
+//   const parsedBody = JSON.parse(body);
+//   // TODO definir que va aca
+//   console.log(parsedBody.isPublicMode);
+//   shard = parsedBody.isPublicMode ? shards.shard1 : shards.shard2;
+// });
 
-  // TODO aplicar criterio de decision para que shard lo mandas
-  routingTable.set(key, shards.shard1);
-  return shards.shard1;
-};
+// if (routingTable.has(key)) {
+//   return routingTable.get(key);
+// }
+
+// // TODO aplicar criterio de decision para que shard lo mandas
+// routingTable.set(key, shards.shard1);
+// return shards.shard1;
 
 module.exports = {
   pickShard,
