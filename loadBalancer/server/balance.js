97a5d95211fc539461f5de0a6f9d476df08dc7a6
Unknown <matiasblacutt16@gmail.com>
2021-07-22 13:28:25 -0300

    Cambio las url a la del load balancer y  comparo en el load balancer con un string  de 'true' para saber si se esta en modo publico o privado

--- loadBalancer/server/balance.js
+++ loadBalancer/server/balance.js
@@ -1,6 +1,6 @@
 const shards = require('./shards.json');
 
-const pickShard = (req) => (req.headers.ispublicmode ? shards.shard1 : shards.shard2);
+const pickShard = (req) => ((req.headers.ispublicmode == "true") ? shards.shard1 : shards.shard2);
 
 module.exports = {
   pickShard,
